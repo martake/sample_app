5098f12faba71d96a1b439a4b7e4015d8265f5b1
Mariko Takeuchi <martake@Adam.local>
2014-07-24 12:12:48 +0900

    Mail Settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
+  :address              => "smtp.sendgrid.net",
   :port                 => 587,
-  :domain               => "smtp.gmail.com",
-  :user_name            => "test@gmail.com",
-  :password             => "test",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :domain               => "heroku.com",
+  :user_name            => ENV['SENDGRID_USERNAME'],
+  :password             => ENV['SENDGRID_PASSWORD'],
+  :authentication       => "plain"
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.default_url_options[:host] = Rails.env.development? ? "localhost:3000" : "https://enigmatic-sands-1609.herokuapp.com"
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "test@gmail.com"
+    message.to = "example@gmail.com"
   end
 end
